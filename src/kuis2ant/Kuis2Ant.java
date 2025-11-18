04952d13f22f37e176fb28f8327b5bdeae79de8e
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2025-11-18 16:59:41 +0700

    new file:   build.xml 	new file:   build/built-jar.properties 	new file:   build/classes/kuis2ant/GUIGameFrame$1.class 	new file:   build/classes/kuis2ant/GUIGameFrame$2.class 	new file:   build/classes/kuis2ant/GUIGameFrame$3.class 	new file:   build/classes/kuis2ant/GUIGameFrame.class 	new file:   build/classes/kuis2ant/Kuis2Ant.class 	new file:   dist/Kuis2Ant.jar 	new file:   dist/README.TXT 	new file:   manifest.mf 	new file:   nbproject/build-impl.xml 	new file:   nbproject/genfiles.properties 	new file:   nbproject/private/private.properties 	new file:   nbproject/project.properties 	new file:   nbproject/project.xml 	new file:   src/kuis2ant/GUIGameFrame.form 	new file:   src/kuis2ant/GUIGameFrame.java 	new file:   src/kuis2ant/Kuis2Ant.java

---
+++ src/kuis2ant/Kuis2Ant.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kuis2ant;
+
+/**
+ *
+ * @author Areep
+ */
+public class Kuis2Ant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(() -> new GUIGameFrame().setVisible(true));
+    }
+    
+}
